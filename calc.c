891da5df8ee81d038e5b31066600020e7c2b53bd
nik3553 <nikitatilekar33@gmail.com>
2019-12-05 07:25:38 -0800

    calc

---
+++ calc.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+int a,b,c=0,d=0,e=0;
+printf("enter two numbers");
+scanf("%d%d",&a,&b);
+c=a+b;
+printf("add:",c);
+d=a-b;
+printf("sub:",d);
+e=a*b;
+printf("mul:",e);
+
+return 0;
+}
