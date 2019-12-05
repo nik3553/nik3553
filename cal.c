bb84ffc3f6c20d739fc79513bd04a722f6f7ee82
nik3553 <nikitatilekar33@gmail.com>
2019-12-05 06:29:33 -0800

    Commiting to the gitHUB

---
+++ cal.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+int a,b,c=0,d=0,e=0;
+printf("enter two numbers");
+scanf("%d,%d",&a,&b);
+c=a+b;
+printf("addition:",c);
+d=a-b;
+printf("sub:",d);
+e=a*b;
+printf("mul:",e);
+return 0;
+}
